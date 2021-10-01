364482493255c150444553f8d438b0f0f98298a0
Hyperloll <90119351+Hyperloll@users.noreply.github.com>
2021-09-30 20:11:37 -0400

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -55,11 +55,23 @@ let questions = [
       "30"
     ]
   },
+    
+  {
+     numb: 6,
+     question: "coolkdi",
+     answer: "Option 1",
+     options: [
+       "Option 1",
+       "option 2",
+       "option 3",
+       "option 4"
+     ]
+   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
   //   {
-  //   numb: 6,
+  //   numb: 7,
   //   question: "Your Question is Here",
   //   answer: "Correct answer of the question is here",
   //   options: [
